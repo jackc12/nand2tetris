35896ea585efb1ee1817f6f23064ad8c98686b92
jack <jack.conway@blockitnow.com>
2026-03-29 18:16:35 -0400

    i suck at c

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -1,1 +1,13 @@
-int main(int argc, char *argv[]) { return 0; }
+#include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2)
+    return 0;
+  else {
+    printf("\n\n%s\n\n", argv[1]);
+    return 0;
+  }
+}
