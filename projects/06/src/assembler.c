ea12bc86991f9057c58c878c3a5498d1307f6b22
jack <jack.conway@blockitnow.com>
2026-03-29 18:55:24 -0400

    nonsense

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -9,13 +9,16 @@ int main(int argc, char *argv[]) {
   else {
     int instruction_count, current_instruction = 0, line_number = 0;
     char **lines = constructor(argv[1], &instruction_count);
-    while(has_more_lines(line_number, instruction_count) {
-		    printf("%s", current_instruction)
-		    advance(&lines, &current_instruction, &instruction_count)
-		    printf("%s", current_instruction)
-
-		    }
-    free_line_array(lines, instruction_count)
+    char *instruction;
+    while (has_more_lines(line_number, instruction_count)) {
+      printf("%d", current_instruction);
+      printf("%s", instruction);
+      instruction = advance(lines, current_instruction, instruction_count);
+      printf("%d", current_instruction);
+      printf("%s", instruction);
+      line_number = current_instruction;
+    }
+    free_line_array(lines, instruction_count);
   }
   return 0;
 }
