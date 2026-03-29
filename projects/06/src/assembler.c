706242c216d602fb4333b209f1943c1687048ca1
jack <jack.conway@blockitnow.com>
2026-03-29 19:25:39 -0400

    seg fault

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -1,3 +1,4 @@
+#include "code.h"
 #include "parser.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +12,7 @@ int main(int argc, char *argv[]) {
     // fix later
     int instruction_count, line_number = -1;
     char **lines = constructor(argv[1], &instruction_count);
-    char *instruction = NULL;
+    char *instruction = NULL, *dest, *comp, *jump;
     while (has_more_lines(line_number, instruction_count)) {
       instruction = advance(lines, line_number, instruction_count);
       switch (instruction_type(instruction)) {
@@ -23,6 +24,16 @@ int main(int argc, char *argv[]) {
       case C_INSTRUCTION:
         // TODO
         printf("C_INSTRUCTION\n");
+        dest = parser_dest(instruction);
+        dest = code_dest(instruction);
+        comp = parser_comp(instruction);
+        comp = code_comp(instruction);
+        jump = parser_jump(instruction);
+        jump = code_jump(instruction);
+        instruction = strcat("111", dest);
+        instruction = strcat(instruction, comp);
+        instruction = strcat(instruction, jump);
+        printf("i: %s\n", instruction);
         break;
 
       case L_INSTRUCTION:
@@ -30,8 +41,6 @@ int main(int argc, char *argv[]) {
         printf("L_INSTRUCTION\n");
         break;
       }
-      printf("ln: %d\n", line_number);
-      printf("i: %s\n", instruction);
       line_number++;
     }
     free_line_array(lines, instruction_count);
