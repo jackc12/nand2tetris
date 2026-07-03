131706859312f9448a0b6f34c96c880287224e99
jack <jack.conway@blockitnow.com>
2026-07-03 17:35:47 -0400

    clean

--- projects/06/Prog.asm
+++ projects/06/Prog.asm
@@ -1,8 +1,12 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
 
-@9
+// Computes R0 = 2 + 3  (R0 refers to RAM[0])
 
-// comment
-M=1
-D=M
-D;JGT
-M=M+1
+@2
+D=A
+@3
+D=D+A
+@0
+M=D
