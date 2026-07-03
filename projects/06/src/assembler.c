4ae7fa114cdd7d64fca991d1fa45f92df8af11b5
jack <jack.conway@blockitnow.com>
2026-07-03 17:37:06 -0400

    rm duplicate

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -60,17 +60,3 @@ int main(int argc, char *argv[]) {
   }
   return 0;
 }
-
-void int_to_bin(char *a_instruction, char *binary, size_t binary_size) {
-  int integer = atoi(a_instruction);
-  int current_len = 0;
-  for (int i = 15; i >= 0; i--) {
-    int remaining_space = binary_size - current_len;
-    int written = snprintf(binary + current_len, remaining_space, "%d",
-                           (integer >> i) & 1);
-    current_len += written;
-    if (current_len >= (int) binary_size) {
-      break;
-    }
-  }
-}
