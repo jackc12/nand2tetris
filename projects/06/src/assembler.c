7671baa129c8cb5bc86af9cdd77acdddf202c41b
jack <jack.conway@blockitnow.com>
2026-07-03 17:20:44 -0400

    bunch of errors

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -70,7 +70,7 @@ void int_to_bin(char *a_instruction, char *binary, size_t binary_size) {
     int written = snprintf(binary + current_len, remaining_space, "%d",
                            (integer >> i) & 1);
     current_len += written;
-    if (current_len >= (int) binary_size) {
+    if (current_len >= (int)binary_size) {
       break;
     }
   }
---
+++ projects/06/tests/assembler_test.c
@@ -0,0 +1,18 @@
+#include "assembler.h"
+#include "minunit.h"
+#include <stddef.h>
+
+MU_TEST(int_to_bin_test) {
+  char *binary[16] = '';
+  size_t binary_size = sizeof(binary);
+  char nine[16] = "9", twelve[16] = "12", twelve_k[16] = "12000";
+  int_to_bin(nine, binary, binary_size);
+  mu_assert_string_eq("0000000000001001", hack);
+}
+MU_TEST_SUITE(test_suite) { MU_RUN_TEST(int_to_bin_test); }
+
+int main() {
+  MU_RUN_SUITE(test_suite);
+  MU_REPORT();
+  return MU_EXIT_CODE;
+}
