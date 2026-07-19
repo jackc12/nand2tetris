f8c5ed08a31ddbe549c19632da79febbd3fc5805
jack <jack.conway@blockitnow.com>
2026-07-19 16:01:16 -0400

    saving

--- projects/06/src/symbol_table.c
+++ projects/06/src/symbol_table.c
@@ -9,21 +9,30 @@
  */
 SymbolTable *constructor() {
   SymbolTable *symbol_table = NULL;
-  char *keys[] = {"foenem", "twin"};
-  int values[] = {0, 1};
+  struct {
+    char *key;
+    int value;
+  } predefined_symbols[] = {{"R0", 0},   {"R1", 1},   {"R2", 2},   {"R3", 3},
+                            {"R4", 4},   {"R5", 5},   {"R6", 6},   {"R7", 7},
+                            {"R8", 8},   {"R9", 9},   {"R10", 10}, {"R11", 11},
+                            {"R12", 12}, {"R13", 13}, {"R14", 14}, {"R15", 15}};
 
-  for (int i = 0; i < 2; i++) {
+  int element_count =
+      sizeof(predefined_symbols) / sizeof(predefined_symbols[0]);
+  for (int i = 0; i < element_count; i++) {
     SymbolTable *item = (SymbolTable *)malloc(sizeof(SymbolTable));
-    if (item == NULL) {
-      perror("Failed to allocate memory");
-      exit(EXIT_FAILURE);
-    }
-    strcpy(item->key, keys[i]);
-    item->value = values[i];
+    if (item == NULL)
+      return NULL;
+
+    // Safely copy the key from the tuple
+    strncpy(item->key, predefined_symbols[i].key, sizeof(item->key) - 1);
+    item->key[sizeof(item->key) - 1] = '\0';
+
+    // Grab the value from the tuple
+    item->value = predefined_symbols[i].value;
 
     HASH_ADD_STR(symbol_table, key, item);
   }
-
   return symbol_table;
 }
 
@@ -32,7 +41,7 @@ int main() {
 
   // Look up a value
   SymbolTable *result = NULL;
-  HASH_FIND_STR(my_table, "foenem", result);
+  HASH_FIND_STR(my_table, "R1", result);
 
   if (result) {
     printf("Found 'b': %d\n", result->value);
