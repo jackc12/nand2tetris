e549777f6f19bd489c97702aef416ad72d72a52c
jack <jack.conway@blockitnow.com>
2026-07-19 15:19:09 -0400

    fix order

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -1,9 +1,9 @@
-#include "code.h"
-#include "parser.h"
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "code.h"
+#include "parser.h"
 
 void int_to_bin(char *a_instruction, char *binary, size_t binary_size);
 
