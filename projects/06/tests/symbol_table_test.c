2deab15b8a73fe56601fccabdc1e9dd3b92625fa
Jack Conway <jack.conway@blockitnow.com>
2026-08-11 16:14:47 -0400

    symbol test

--- projects/06/tests/symbol_table_test.c
+++ projects/06/tests/symbol_table_test.c
@@ -2,9 +2,9 @@
 #include "symbol_table.h"
 
 MU_TEST(contains_test) {
-  //SymbolTable *symbol_table = constructor();
-  //int contains_ = contains(symbol_table, "R0");
-  //mu_assert_int_eq(1, contains_);
+  SymbolTable *symbol_table = constructor();
+  int contains_ = contains(symbol_table, "R0");
+  mu_assert_int_eq(1, contains_);
 }
 MU_TEST_SUITE(test_suite) { MU_RUN_TEST(contains_test); }
 
