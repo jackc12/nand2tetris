2a598b10864c4946c44bd427b2353f99a7778676
Jack Conway <jack.conway@blockitnow.com>
2026-08-12 10:36:10 -0400

    rename constructor

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
     if (!file)
       return 1;
 
+    SymbolTable *symbol_table = new_symbol_table();
     while (has_more_lines(line_number, instruction_count)) {
     }
     char **hack = malloc(instruction_count * sizeof(char *));
