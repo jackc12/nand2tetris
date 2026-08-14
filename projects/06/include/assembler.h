c228a03ac1c838fd3c93a396f78231c7fac28c6e
Jack Conway <jack.conway@blockitnow.com>
2026-08-14 13:14:28 -0400

    writing trst

--- projects/06/include/assembler.h
+++ projects/06/include/assembler.h
@@ -2,5 +2,7 @@
 #define ASSEMBLER_H
 #include <stddef.h>
 void int_to_bin(char *a_instruction, char *binary, size_t binary_size);
+void first_pass(char *symbol_table, char **lines, int line_number,
+                int instruction_count);
 
 #endif
--- projects/06/tests/assembler_test.c
+++ projects/06/tests/assembler_test.c
@@ -13,7 +13,11 @@ MU_TEST(int_to_bin_test) {
   int_to_bin(twelve_k, binary, binary_size);
   mu_assert_string_eq("0010111011100000", binary);
 }
-MU_TEST_SUITE(test_suite) { MU_RUN_TEST(int_to_bin_test); }
+MU_TEST(first_pass_test) {}
+MU_TEST_SUITE(test_suite) {
+  MU_RUN_TEST(int_to_bin_test);
+  MU_RUN_TEST(first_pass_test);
+}
 
 int main() {
   MU_RUN_SUITE(test_suite);
