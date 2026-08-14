96fbf5c30c75228b0747a673f179cc60a3bd1212
Jack Conway <jack.conway@blockitnow.com>
2026-08-14 18:13:09 -0400

    Max.asm

--- projects/06/Prog.asm
+++ projects/06/Prog.asm
@@ -1,12 +1,30 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
+// File name: projects/6/max/Max.asm
 
-// Computes R0 = 2 + 3  (R0 refers to RAM[0])
+// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
+// Usage: Before executing, put two values in R0 and R1.
 
-@2
-D=A
-@3
-D=D+A
-@0
-M=D
+  // D = R0 - R1
+  @R0
+  D=M
+  @R1
+  D=D-M
+  // If (D > 0) goto ITSR0
+  @ITSR0
+  D;JGT
+  // Its R1
+  @R1
+  D=M
+  @OUTPUT_D
+  0;JMP
+(ITSR0)
+  @R0
+  D=M
+(OUTPUT_D)
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
