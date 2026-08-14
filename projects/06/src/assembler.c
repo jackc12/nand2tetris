ff3a777b4316acff780274faa76dd53a6185c596
Jack Conway <jack.conway@blockitnow.com>
2026-08-14 15:43:39 -0400

    ok I think

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -73,12 +73,12 @@ int main(int argc, char *argv[]) {
       }
       line_number++;
     }
+    printf("i: %d\n", get_address(symbol_table, "i"));
+    printf("sum: %d\n", get_address(symbol_table, "sum"));
     destroy_symbol_table(&symbol_table);
     free_line_array(lines, instruction_count);
     free_line_array(hack, instruction_number);
     fclose(file);
-    printf("i: %d\n", get_address(symbol_table, "i"));
-    printf("sum: %d\n", get_address(symbol_table, "sum"));
   }
   return 0;
 }
