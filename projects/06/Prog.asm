6a6e8d9cc1152919a732e187362e32061d4f97e2
Jack Conway <jack.conway@blockitnow.com>
2026-08-14 15:48:30 -0400

    Max.asm

--- projects/06/Prog.asm
+++ projects/06/Prog.asm
@@ -1,33 +1,30 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/6/rect/RectL.asm
+// File name: projects/6/max/Max.asm
 
-// Symbol-less version of the Rect.asm program.
-// Designed for testing the basic version of the assembler.
+// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
+// Usage: Before executing, put two values in R0 and R1.
 
-@0
-D=M
-@23
-D;JLE
-@16
-M=D
-@16384
-D=A
-@17
-M=D
-@17
-A=M
-M=-1
-@17
-D=M
-@32
-D=D+A
-@17
-M=D
-@16
-DM=M-1
-@10
-D;JGT
-@23
-0;JMP
+  // D = R0 - R1
+  @R0
+  D=M
+  @R1
+  D=D-M
+  // If (D > 0) goto ITSR0
+  @ITSR0
+  D;JGT
+  // Its R1
+  @R1
+  D=M
+  @OUTPUT_D
+  0;JMP
+(ITSR0)
+  @R0
+  D=M
+(OUTPUT_D)
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
