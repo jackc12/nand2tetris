b5533f3e2a775e6cf21fdf84fbb205741d1060d4
Jack Conway <jack.conway@blockitnow.com>
2026-08-14 15:42:34 -0400

    saving

--- projects/06/src/assembler.c
+++ projects/06/src/assembler.c
@@ -42,8 +42,6 @@ int main(int argc, char *argv[]) {
         } else {
           address = get_address(symbol_table, a_instruction);
 
-          printf("a_instruction: %s\n", a_instruction);
-          printf("address: %d\n", address);
           if (address == -1) {
             add_entry(&symbol_table, a_instruction, free_address);
             snprintf(a_instruction, 16, "%d", free_address);
@@ -79,8 +77,8 @@ int main(int argc, char *argv[]) {
     free_line_array(lines, instruction_count);
     free_line_array(hack, instruction_number);
     fclose(file);
-    // printf("i: %d\n", get_address(symbol_table, "i"));
-    // printf("sum: %d\n", get_address(symbol_table, "sum"));
+    printf("i: %d\n", get_address(symbol_table, "i"));
+    printf("sum: %d\n", get_address(symbol_table, "sum"));
   }
   return 0;
 }
